13cd2029bdf4b8c472830b24b45ab61194918b74
A9ata <a9ata9er@gmail.com>
2024-10-11 14:35:45 +0300

    Adding img, working with App.jsx visuals

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,102 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/nintendo-switch.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <h1>Game Console Nintendo Switch</h1>
+          <div className='flex'>
+              <div className="button--left">
+                  <div className='flex-grow'>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          Left
+                      </button>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          Volume Dowm
+                      </button>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          L-Joystick
+                      </button>
+                  </div>
+                  <div>
+                      <div>
+                          <button onClick={() => setCount((count) => count + 1)}>
+                              Up
+                          </button>
+                      </div>
+                      <div>
+                          <button onClick={() => setCount((count) => count + 1)}>
+                              Right
+                          </button>
+                          <button onClick={() => setCount((count) => count + 1)}>
+                              Down
+                          </button>
+                      </div>
+                      <div>
+                          <button onClick={() => setCount((count) => count + 1)}>
+                              Left
+                          </button>
+                      </div>
+                  </div>
+                  <div>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          Screenshot
+                      </button>
+                  </div>
+              </div>
+              <div>
+                  <button onClick={() => setCount((count) => count + 1)}>
+                      On/Off
+                  </button>
+                  <div target="_blank">
+                      <img src={viteLogo} className='w-[833px] h-[344.61px]' alt="Vite logo"/>
+                  </div>
+              </div>
+              <div className="button--right">
+                  <div>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          Right
+                      </button>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          Volume Up
+                      </button>
+                      <div>
+                          <div>
+                              <button onClick={() => setCount((count) => count + 1)}>
+                                  X
+                              </button>
+                          </div>
+                          <div>
+                              <button onClick={() => setCount((count) => count + 1)}>
+                                  A
+                              </button>
+                              <button onClick={() => setCount((count) => count + 1)}>
+                                  B
+                              </button>
+                          </div>
+                          <div>
+                              <button onClick={() => setCount((count) => count + 1)}>
+                                  Y
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+                  <div>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          R-Joystick
+                      </button>
+                  </div>
+                  <div>
+                      <button onClick={() => setCount((count) => count + 1)}>
+                          Home
+                      </button>
+                  </div>
+              </div>
+          </div>
+      </>
   )
 }
 
